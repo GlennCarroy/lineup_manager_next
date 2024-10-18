268fd642a8fba2aab7a80916b53dd8753d657b81
Glenn CARROY <gcarroy@cbtw.tech>
2024-10-18 17:54:04 +0200

    readme and home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,71 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="flex items-center justify-center overflow-hidden z-50 ">
+      <div className="mx-auto h-full px-4  pb-20   md:pb-10 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+        <div className="flex flex-col items-center justify-between lg:flex-row py-16">
+            <div>
+                <div className="lg:max-w-xl lg:pr-5 z-40">
+                    <h2 className="mb-6 max-w-lg text-5xl font-light leading-snug tracking-tight text-g1 sm:text-7xl sm:leading-snug">
+                        Lineup Manager for
+                        <span className="my-1 inline-block border-b-8 border-g4 bg-white px-4 font-bold text-g4 animate__animated animate__flash">roller derby</span>
+                    </h2>
+                    <p className="text-base text-gray-700">Manage your team in game.</p>                
+                </div>
+            </div>
+            <div className="lg:ml-32 lg:block lg:w-1/2 xs:mt-3">
+
+                <div className="abg-orange-400 mx-auto w-fit overflow-hidden rounded-[4rem] rounded-br-none rounded-tl-none">
+                    <div className="relative flex flex-col mt-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+                        <div className="p-4">
+                            <h5 className="mb-2 text-slate-800 text-xl font-semibold">
+                                Build your charter
+                            </h5>
+                            <p className="text-slate-600 leading-normal font-light">
+                                Manage all your players in one page
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="abg-orange-400 mx-auto w-fit overflow-hidden rounded-[4rem] rounded-br-none rounded-tl-none">
+                    <div className="relative flex flex-col mt-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+                        <div className="p-4">
+                            <h5 className="mb-2 text-slate-800 text-xl font-semibold">
+                                In game management
+                            </h5>
+                            <p className="text-slate-600 leading-normal font-light">
+                                Build a roster, start a game, track penalties
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="abg-orange-400 mx-auto w-fit overflow-hidden rounded-[4rem] rounded-br-none rounded-tl-none">
+                    <div className="relative flex flex-col mt-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+                        <div className="p-4">
+                            <h5 className="mb-2 text-slate-800 text-xl font-semibold">
+                                Jam by jam data
+                            </h5>
+                            <p className="text-slate-600 leading-normal font-light">
+                                Send your lines, pick the lead and get some data about how good your team is doing during the game.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div className="flex items-center justify-center overflow-hidden gap-6">
+          <Link 
+            href="/players" 
+            className="rounded-md bg-gray-900 px-3 py-2 text-xl text-white" 
+            aria-current="page"
+            >
+             Start managing charter 
+          </Link>
+          <a href="https://www.youtube.com/watch?v=cjkJfjTe-Ds&t=27s" target="_blank" className='rounded-md px-3 py-2 text-xl font-medium border-2 border-cyan-400 space-x-4 block text-cyan-400'>New to roller derby ?</a>
+        </div>
+      </div>  
     </div>
   );
 }
